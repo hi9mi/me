525444be4643faefb4bdd03f012ea51183da0c81
Roman <4elove4ek40@gmail.com>
2023-11-30 22:35:14 +0600

    update astro config base url

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,8 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hi9mi.github.io',
+  base: '/me',
   integrations: [UnoCSS({
     injectReset: "@unocss/reset/tailwind-compat.css"
   }), vue()]
