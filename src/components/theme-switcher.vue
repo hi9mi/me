a92a5fc1fbb9bd5a159616e4da62329cbb2ded86
Roman <4elove4ek40@gmail.com>
2023-12-01 11:20:10 +0600

    feat added index page

--- src/components/theme-switcher.vue
+++ src/components/theme-switcher.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { ref, watchEffect } from "vue"
+import { ref, watchEffect } from "vue";
 
 const theme = ref(globalThis.localStorage.getItem("theme") ?? "light");
 
 const toggle = () => {
-  theme.value = theme.value === 'dark' ? "light" : "dark"
+  theme.value = theme.value === "dark" ? "light" : "dark";
 };
 
 watchEffect(() => {
@@ -15,15 +15,42 @@ watchEffect(() => {
   }
   localStorage.setItem("theme", theme.value);
 });
-
 </script>
 
 <template>
-  <button @click="toggle" title="Toggle theme" id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-gray-200 dark:focus-visible:ring-gray-700 rounded-lg text-sm p-2.5 h-max">
-    <svg aria-hidden="true" id="theme-toggle-dark-icon" class="w-5 h-5 " :class="{'hidden': theme === 'light' }" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
-    <svg aria-hidden="true" id="theme-toggle-light-icon" class="w-5 h-5" :class="{'hidden': theme === 'dark' }" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-    <span class="sr-only">
-      Switch theme to {{ theme === 'dark' ? 'light' : 'dark'}}
-    </span>
-</button>
+  <button
+    @click="toggle"
+    title="Toggle theme"
+    id="theme-toggle"
+    type="button"
+    class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-gray-200 dark:focus-visible:ring-gray-700 rounded-lg text-sm p-2.5 h-max"
+  >
+    <svg
+      aria-hidden="true"
+      id="theme-toggle-dark-icon"
+      class="w-5 h-5"
+      :class="{ hidden: theme === 'light' }"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+    </svg>
+    <svg
+      aria-hidden="true"
+      id="theme-toggle-light-icon"
+      class="w-5 h-5"
+      :class="{ hidden: theme === 'dark' }"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+    <span class="sr-only">Switch theme to {{ theme === "dark" ? "light" : "dark" }}</span>
+  </button>
 </template>
