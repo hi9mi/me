d98ac191e602a7833f0580e5eaed5972470032ed
Roman <4elove4ek40@gmail.com>
2024-06-04 11:46:49 +0500

    feat: added envs to astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
+import process from 'node:process'
+
 import vue from '@astrojs/vue'
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hi9mi.github.io',
-  base: '/me',
+  site: process.env.SITE || 'https://hi9mi.github.io',
+  base: process.env.BASE || '/me',
   integrations: [
     UnoCSS({
       injectReset: '@unocss/reset/tailwind-compat.css',
